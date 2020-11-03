3e98e810017907a00c2c389107096bcbdba5a323
Brittani Baker <brittanibaker07@gmail.com>
2020-11-03 20:48:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
-def square_array(array)
-  # your code here
+require 'pry' 
+def square_array(numbers)
+  squared = [1,4,9]
+  count = 0
+  while count < numbers.length do
+    numbers[count]**2
+    count += 1
+    squared.push(numbers[count])
+    
+    count += 1
+  end
+  squared
 end
